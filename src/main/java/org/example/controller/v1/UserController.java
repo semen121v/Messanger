5e9d686856d39e6c7d688ce7c4ba3b075b3eb998
semenkravcov <s.kravtsov@platformaofd.ru>
2026-03-09 23:52:28 +0300

    NM-3: настроил создание пользователя

--- src/main/java/org/example/controller/v1/UserController.java
+++ src/main/java/org/example/controller/v1/UserController.java
@@ -1,5 +1,10 @@
 package org.example.controller.v1;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
 import org.example.model.dto.request.UserCreateDtoRequest;
 import org.example.model.dto.response.UserDtoResponse;
 import org.example.service.api.UserService;
@@ -8,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
+@Api(tags = "Пользователи", description = "API для управления пользователями")
 public class UserController {
 
     private final UserService service;
@@ -20,6 +26,12 @@ public class UserController {
      * Создание нового пользователя
      */
     @PostMapping
+    @Operation(summary = "Создание нового пользователя", description = "Создаёт нового пользователя в системе")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Пользователь успешно создан"),
+            @ApiResponse(code = 400, message = "Некорректные данные"),
+            @ApiResponse(code = 409, message = "Email или телефон уже существует")
+    })
     public ResponseEntity<Long> createUser(@RequestBody UserCreateDtoRequest request) {
         return ResponseEntity.ok(service.createUser(request));
     }
@@ -28,6 +40,7 @@ public class UserController {
      * Получение пользователя по ID
      */
     @GetMapping("/{id}")
+    @Operation(summary = "Получить пользователя по id")
     public ResponseEntity<UserDtoResponse> getUserById(@PathVariable Long id) {
         return ResponseEntity.ok(service.getUser(id));
     }
